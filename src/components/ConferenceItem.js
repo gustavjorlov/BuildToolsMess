9fa496e3d2e90d37286c5a40ab4552a2e1af34f8
Gustav Jorlöv <gustav.jorlov@kits.se>
2016-03-01 17:35:31 +0100

    code cleanup

--- src/components/ConferenceItem.js
+++ src/components/ConferenceItem.js
@@ -14,16 +14,3 @@ export default class ConferenceItem extends React.Component{
 			</div>);
 	}
 }
-
-/*
-<div className={"person" + (this.props.item.presentation ? ' presentation' : '')}>
-				<div className="ball">
-					<img src={"images/" + this.props.item.image}/>
-				</div>
-				<div className="metadata">
-					<p className="duration">{this.props.item.time}</p>
-					<h3 className="title">{this.props.item.title}</h3>
-					<p className="topic">{this.props.item.topic}</p>
-				</div>
-			</div>
-*/
